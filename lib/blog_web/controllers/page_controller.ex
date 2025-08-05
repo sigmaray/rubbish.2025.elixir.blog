afdf50a4ab1f6595784c7d2c8371207b1e2edbfc
Stanislav Zaleski <sigmaray@gmail.com>
2025-08-05 15:39:08 +0200

    redirect(conn, to: ~p"/posts")

--- lib/blog_web/controllers/page_controller.ex
+++ lib/blog_web/controllers/page_controller.ex
@@ -2,8 +2,9 @@ defmodule BlogWeb.PageController do
   use BlogWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    redirect(conn, to: ~p"/posts")
+    # # The home page is often custom made,
+    # # so skip the default app layout.
+    # render(conn, :home, layout: false)
   end
 end
